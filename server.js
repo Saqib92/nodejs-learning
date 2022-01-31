726568499eb388f035ace50f349f8bedef80bd04
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-01-31 18:38:46 +0500

    remove sub directories, employee model added, code cleanup, cors options added

--- server.js
+++ server.js
@@ -2,32 +2,30 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const cors = require('cors');
-
+const corsOptions = require('./src/config/corsOptions');
 
 const PORT = process.env.PORT || 3500;
 
 //Built In middlewares 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions)); // FOR Cors
+app.use(express.urlencoded({ extended: false })); //FOR Urlencoded form data
+app.use(express.json()); //FOR JSON
 
 //for static Images and Files
 app.use(express.static(path.join(__dirname, './src/')));
-app.use('/subdir', express.static(path.join(__dirname, './src/'))); //For sub dir static files
 
 //Routes
 app.use('/', require('./src/routes/root')); // root stack router
-app.use('/subdir', require('./src/routes/subdir')); //sub directory  router
 app.use('/employees', require('./src/routes/api/employees'));
 
 
 app.all('*', (req, res) => {
   res.status(404);
-  if(req.accepts('html')){
+  if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, 'src/views', '404.html'));
-  }else if(req.accepts('json')){
-    res.json({error:'404 Not Found'});
-  }else{
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' });
+  } else {
     res.type('txt').send('404 Not Found');
   }
 })
--- src/routes/api/employees.js
+++ src/routes/api/employees.js
@@ -1,36 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-//For Dummy Data
-const data = {};
-data.employees = require('../../assets/data/employees.json');
+const employeesController = require('../../controllers/employeesController')
 
 router.route('/')
-    .get((req, res) => {
-        res.json(data.employees);
-    })
-    .post((req, res) => {
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname
-        });
-    })
-    .put((req, res) => {
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname
-        });
-    })
-    .delete((req, res) => {
-        res.json({
-            "id": req.body.id
-        })
-    });
-
+    .get(employeesController.getAllEmployees)
+    .post(employeesController.createNewEmployee)
+    .put(employeesController.updateEmployee)
+    .delete(employeesController.deleteEmployee);
 
 router.route('/:id')
-    .get((req, res) => {
-        res.json({ "id": req.params.id });
-    });
+    .get(employeesController.getEmployee);
 
 module.exports = router;
