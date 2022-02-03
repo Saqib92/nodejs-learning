7cdc53bcc0ec2a002ba544f485a05b38d52c17e5
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-02-03 17:06:06 +0500

    env update, JWT Implementation, middleware created

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,20 +3,43 @@ const usersDB = {
     setUsers: function (data) { this.users = data }
 };
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const fsPromises = require('fs').promises;
 const path = require('path');
-const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 const handleLogin = async (req, res) => {
+    console.log(req.body)
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).json({ 'message': 'Username and Password are Required' });
     const foundUser = usersDB.users.find(person => person.email == email);
-    if (!foundUser) return res.sendStatus(401); //Unauthorized
-
+    if (!foundUser) return res.sendStatus(401); //Unauthorized || User Not Found
+    console.log(foundUser);
     // check password with bcrypt
     const match = await bcrypt.compare(password, foundUser.password);
     if (match) {
-        res.json({ message: 'User Login Successfull', data: foundUser })
+        //create JWTs
+        const accessToken = jwt.sign(
+            { 'email': foundUser.email },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        );
+        const refreshToken = jwt.sign(
+            { 'email': foundUser.email },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        const otherUsers = usersDB.users.filter(person => person.email != foundUser.email);
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+        res.json({ accessToken });
     }
     else {
         res.sendStatus(401);
