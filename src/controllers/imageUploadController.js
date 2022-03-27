231f8735787703797f80b70b3809c5dcd15f4921
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-03-27 14:18:25 +0500

    img upload clean

--- src/controllers/imageUploadController.js
+++ src/controllers/imageUploadController.js
@@ -1,34 +1,6 @@
 const path = require('path');
 
 const handelImage = async (req, res, next) => {
-    // try {
-    //     if (!req.files || Object.keys(req.files).length === 0) {
-    //         return res.status(400).send('No files were uploaded.');
-    //     } else {
-    //         let data = [];
-    //         //loop all images
-    //         req.files.images.forEach((image) => {
-    //             // move image to uploads directory
-    //             uploadPath = path.join(__dirname, '..', '..', 'uploads/' + Date.now() + image.name);
-    //             image.mv(uploadPath);
-    //             // push image details
-    //             data.push({
-    //                 name: image.name,
-    //                 mimeType: image.mimetype,
-    //                 size: image.size
-    //             });
-    //         });
-
-    //         //return response
-    //         res.send({
-    //             success: true,
-    //             message: 'Images uploaded!',
-    //             data: data
-    //         });
-    //     }
-    // } catch (err) {
-    //     res.status(500).send(err);
-    // }
     if (!req.files) {
         return res.send({
             success: false,
@@ -51,7 +23,7 @@ const handelImage = async (req, res, next) => {
                 message: 'upload error'
             });
         }
-        
+
         data.push({
             name: image.name,
             mimeType: image.mimetype,
