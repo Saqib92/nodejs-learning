9d7461739e9a05c9cb5ec4ef751e8ce9b29a3e31
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-03-28 15:23:57 +0500

    Update allowedOrigins.js

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:3500',
     'http://localhost:3500',
     'http://localhost:8100',
-    'http://localhost/'
+    'http://localhost',
 ];
 
 module.exports = allowedOrigins;
