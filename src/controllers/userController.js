763b36972a5ae4b4ea1a84208a16b45eb40a453e
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-05-25 13:28:34 +0500

    reward Video added and user coins added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,12 +3,13 @@ const User = require('../model/User');
 const getUserByToken = async (req, res) => {
     const cookies = req.headers.authorization.split(' ')[1];
     const Token = cookies;
-    const foundUser = await User.find({ loginToken: Token }).exec();
+    const foundUser = await User.findOne({ loginToken: Token }).exec();
     if (!foundUser) return res.status(401).json({ status: false, message: 'Unauthorized' }); //Unauthorized || User Not Found
     let myUser = {
-        email: foundUser[0].email,
-        fullName: foundUser[0].fullName,
-        _id: foundUser[0]._id
+        email: foundUser.email,
+        fullName: foundUser.fullName,
+        coins: foundUser.coins,
+        _id: foundUser._id
     }
     res.json({ status: true, data: myUser });
 }
@@ -30,60 +31,4 @@ const getAllUsers = async (req, res) => {
 module.exports = {
     getUserByToken,
     getAllUsers
-}
-
-// const createNewEmployee = async (req, res) => {
-//     if (!req?.body?.firstName || !req.body?.lastName) {
-//         return res.status(400).json({ message: 'First and last name is requireq!' })
-//     }
-//     try {
-//         const result = await Employee.create({
-//             firstName: req.body.firstName,
-//             lastName: req.body.lastName
-//         })
-//         return res.json(result);
-//     } catch (err) {
-//         console.error(err)
-//     }
-// }
-
-// const updateEmployee = async (req, res) => {
-//     if (!req?.body?.id) {
-//         return res.status(400).json({ message: 'ID Parameter Required' })
-//     }
-
-//     const employee = await Employee.findOne({ _id: req.body.id }).exec();
-
-//     if (!employee) {
-//         return res.status(204).json({ message: `No Employee Found with Id ${req.body.id}` })
-//     }
-//     if (req.body?.firstName) employee.firstName = req.body.firstName
-//     if (req.body?.lastName) employee.lastName = req.body.lastName
-//     const result = await employee.save();
-//     res.json(result);
-
-// }
-
-// const deleteEmployee = async (req, res) => {
-//     if (!req?.body?.id) {
-//         return res.status(400).json({ message: 'ID Parameter Required' })
-//     }
-//     const employee = await Employee.findOne({ _id: req.body.id }).exec();
-//     if (!employee) {
-//         return res.status(204).json({ message: `No Employee Found with Id ${req.body.id}` })
-//     }
-//     const result = await employee.deleteOne({ _id: req.body.id });
-//     res.json(result);
-// }
-
-// const getEmployee = async (req, res) => {
-//     if (!req?.params?.id) {
-//         return res.status(400).json({ message: 'ID Parameter Required' })
-//     }
-//     const employee = await Employee.findOne({ _id: req.params.id }).exec();
-//     if (!employee) {
-//         return res.status(204).json({ message: `No Employee Found with Id ${req.body.id}` })
-//     }
-//     res.json(employee);
-// }
-
+};
